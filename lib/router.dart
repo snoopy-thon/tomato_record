3303cd967bef3bbde11589098ad9389c3d81582a
Yujin <tkfkd7038@naver.com>
2024-10-01 12:28:45 +0900

    feat: 로그인 여부에 따라 화면 전환 기능 구현
    go_router를 사용하여 화면전환 구현

---
+++ lib/router.dart
@@ -0,0 +1,36 @@
+import 'package:go_router/go_router.dart';
+import 'package:tomato_record/screens/home_screen.dart';
+
+import 'screens/auth_screen.dart';
+import 'screens/error_page.dart';
+import 'screens/splash_screen.dart';
+
+class MyRouter {
+  final loginState;
+  MyRouter(this.loginState);
+
+  late final router = GoRouter(
+    initialLocation: '/',
+    errorBuilder: (context, state) {
+      return ErrorPage(
+        error: state.error,
+      );
+    },
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, GoRouterState state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/auth',
+        builder: (context, GoRouterState state) => const AuthScreen(),
+      ),
+    ],
+    redirect: (context, state) {
+      if (loginState)
+        return '/';
+      else
+        return '/auth';
+    },
+  );
+}
