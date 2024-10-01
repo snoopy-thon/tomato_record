0bbc02d88f7e3f4a3d8082ce9800e0e0a06ecafb
Yujin <tkfkd7038@naver.com>
2024-10-01 15:49:17 +0900

    feat: 위치 마커 이미지 배치하기
    LayoutBuilder를 이용해 실행하는 기기에 맞게 이미지를 배치함

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -6,68 +6,72 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(
-              '토마토마켓',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 50,
-                fontWeight: FontWeight.w500,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        Size size = MediaQuery.of(context).size;
+
+        final imgSize = size.width - 40;
+        final sizeOfPosImg = imgSize * 0.1;
+
+        return SafeArea(
+          child: Scaffold(
+            body: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    '토마토마켓',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      ExtendedImage.asset('assets/imgs/carrot_intro.png'),
+                      Positioned(
+                        width: sizeOfPosImg,
+                        left: imgSize * 0.45,
+                        height: sizeOfPosImg,
+                        top: imgSize * 0.45,
+                        child: ExtendedImage.asset(
+                            'assets/imgs/carrot_intro_pos.png'),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    '우리 동네 중고 직거래 토마토마켓',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const Text(
+                    '토마토마켓은 우리동네 직거래 마켓이에요.\n 내 동네를 설정하고 시작해보세요!',
+                    style: TextStyle(fontSize: 17),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 40,
-            ),
-            Stack(
-              children: [
-                ExtendedImage.asset('assets/imgs/carrot_intro.png'),
-                Positioned(
-                  left: 50,
-                  right: 50,
-                  top: 50,
-                  bottom: 50,
-                  child:
-                      ExtendedImage.asset('assets/imgs/carrot_intro_pos.png'),
-                ),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 40),
-              child: const Text(
-                '우리 동네 중고 직거래 토마토마켓',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w500,
+            bottomNavigationBar: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: FilledButton(
+                style: FilledButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                )),
+                onPressed: () {},
+                child: const Text(
+                  '내 동네 설정하고 시작하기',
                 ),
               ),
             ),
-            const Text(
-              '토마토마켓은 우리동네 직거래 마켓이에요.\n 내 동네를 설정하고 시작해보세요!',
-              style: TextStyle(fontSize: 17),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: FilledButton(
-          style: FilledButton.styleFrom(
-              shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5),
-          )),
-          //backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-          onPressed: () {},
-          child: const Text(
-            '내 동네 설정하고 시작하기',
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
