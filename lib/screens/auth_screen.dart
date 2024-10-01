be6d6ca0a1b6457c04e299c6b4ab9defbf5e0c2d
Yujin <tkfkd7038@naver.com>
2024-10-01 14:31:04 +0900

    feat: Auth Screen에서 intro page 보여주기

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:tomato_record/screens/start/intro_page.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        children: [
-          const Center(
-            child: Text('Auth screen!'),
-          ),
-          Container(
-            color: Colors.accents[2],
-          ),
-          Container(
-            color: Colors.accents[5],
-          ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: PageView(
+          children: [
+            const IntroPage(),
+            Container(
+              color: Colors.accents[2],
+            ),
+            Container(
+              color: Colors.accents[5],
+            ),
+          ],
+        ),
       ),
     );
   }
