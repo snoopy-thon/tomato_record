ab80de01e61b95918a6830cb59d9ece07c13a21f
Yujin <tkfkd7038@naver.com>
2024-10-05 14:52:08 +0900

    feat: appbar에 logout 버튼 생성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tomato_record/states/user_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              context.read<UserProvider>().setUserState(false);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
