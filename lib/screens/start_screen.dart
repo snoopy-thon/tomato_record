651353b2ab41c36088376d7e252e9ed62ff8da5a
Yujin <tkfkd7038@naver.com>
2024-10-08 17:59:13 +0900

    feat: provider를 사용하여 다른 페이지에서 pageController를 접근가능하게 설정

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tomato_record/screens/start/address_page.dart';
 import 'package:tomato_record/screens/start/intro_page.dart';
 
@@ -10,16 +11,22 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: PageView(
-          // physics: const NeverScrollableScrollPhysics(),
-          controller: _pageController,
-          children: [
-            IntroPage(_pageController),
-            AddressPage(),
-            AuthPage(),
-          ],
+    // 일반적으로는 그냥 Provider를 잘 사용하지 않음.
+    // 그러나 우리는 다른 페이지에서 PageController를 접근할 수 있게만 하면 되기때문에 그냥 Provider를 사용.
+    return Provider<PageController>.value(
+      value:
+          _pageController, // value를 사용하면 기존에 생성된 인스턴스를 사용할 수 있음(create는 새로 생성해야됨. 기존거 못씀)
+      child: SafeArea(
+        child: Scaffold(
+          body: PageView(
+            // physics: const NeverScrollableScrollPhysics(),
+            controller: _pageController,
+            children: [
+              IntroPage(),
+              const AddressPage(),
+              const AuthPage(),
+            ],
+          ),
         ),
       ),
     );
