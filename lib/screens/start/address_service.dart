6de088aba4277b7a9fe5174b860f9b7fb2ccfaf7
Yujin <tkfkd7038@naver.com>
2024-10-08 12:44:46 +0900

    feat: 현재 위치와 주변 좌표들을 가져와서 리스트에 추가

--- lib/screens/start/address_service.dart
+++ lib/screens/start/address_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:tomato_record/constants/keys.dart';
 import 'package:tomato_record/data/address_model.dart';
 
+import '../../data/geocode_model.dart';
 import '../../utils/logger.dart';
 
 class AddressService {
@@ -27,22 +28,68 @@ class AddressService {
     return addressModel;
   }
 
-  Future<void> findAddressByCoordinate(
+  Future<List<GeocodeModel>> findAddressByCoordinate(
       {required double log, required double lat}) async {
-    final Map<String, dynamic> formData = {
+    final List<Map<String, dynamic>> formDatas = <Map<String, dynamic>>[];
+
+    formDatas.add({
       'key': VWORLD_KEY,
       'service': 'address',
-      'type': 'BOTH',
+      'type': 'PARCEL',
       'request': 'GetAddress',
       'point': '$log, $lat',
-    };
-    final response = await Dio()
-        .get('https://api.vworld.kr/req/address', queryParameters: formData)
-        .catchError((e) {
-      logger.e(e.message);
     });
 
-    logger.d(response);
-    return;
+    formDatas.add({
+      'key': VWORLD_KEY,
+      'service': 'address',
+      'type': 'PARCEL',
+      'request': 'GetAddress',
+      'point': '${log - 0.01}, $lat',
+    });
+
+    formDatas.add({
+      'key': VWORLD_KEY,
+      'service': 'address',
+      'type': 'PARCEL',
+      'request': 'GetAddress',
+      'point': '${log + 0.01}, $lat',
+    });
+
+    formDatas.add({
+      'key': VWORLD_KEY,
+      'service': 'address',
+      'type': 'PARCEL',
+      'request': 'GetAddress',
+      'point': '$log, ${lat + 0.01}',
+    });
+
+    formDatas.add({
+      'key': VWORLD_KEY,
+      'service': 'address',
+      'type': 'PARCEL',
+      'request': 'GetAddress',
+      'point': '$log, ${lat - 0.01}',
+    });
+
+    List<GeocodeModel> addresses = [];
+
+    for (Map<String, dynamic> formData in formDatas) {
+      final response = await Dio()
+          .get('https://api.vworld.kr/req/address', queryParameters: formData)
+          .catchError((e) {
+        logger.e(e.message);
+      });
+
+      GeocodeModel addressModel =
+          GeocodeModel.fromJson(response.data["response"]);
+
+      if (response.data['response']['status'] == 'OK')
+        addresses.add(addressModel);
+      logger.d(response);
+    }
+
+    logger.d(addresses);
+    return addresses;
   }
 }
