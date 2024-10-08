be660800eeefd6af9d140c1dedddf65d06225dfb
Yujin <tkfkd7038@naver.com>
2024-10-09 01:04:34 +0900

    feat: bottomNavigationBar 골격 생성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,14 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tomato_record/states/user_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          '신봉동',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
         actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+          ),
           IconButton(
             icon: const Icon(Icons.logout),
             onPressed: () {
@@ -17,6 +42,24 @@ class HomeScreen extends StatelessWidget {
             },
           ),
         ],
+      ),
+      body: Center(
+        child: Text('$_selectedIndex'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.apartment), label: '동네생활'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat_outlined), label: '채팅'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '내 정보'),
+        ],
+        //showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        // unselectedItemColor: Colors.grey[350],
+        // selectedItemColor: Theme.of(context).colorScheme.primary,
+        // unselectedLabelStyle: TextStyle(color: Colors.grey[350]),
       ),
     );
   }
