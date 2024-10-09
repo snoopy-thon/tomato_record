4bf0becee6252b44ae5c76106b5190a4fa8030fe
Yujin <tkfkd7038@naver.com>
2024-10-09 17:53:09 +0900

    feat: firebase 연동 시 필요한 것들 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:tomato_record/router.dart';
-import 'package:tomato_record/router/locations.dart';
-import 'package:tomato_record/screens/start_screen.dart';
 import 'package:tomato_record/screens/splash_screen.dart';
+import 'package:tomato_record/states/user_provider.dart';
 import 'package:tomato_record/utils/logger.dart';
-
-import 'screens/home_screen.dart';
 
 void main() {
   logger.d('My first log by logger!!');
   Provider.debugCheckInvalidValueType = null;
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Future.delayed(const Duration(seconds: 1), () => 100),
+        future: _initialization,
         builder: (context, snapshot) {
           return AnimatedSwitcher(
               duration: const Duration(milliseconds: 300),
@@ -32,7 +38,7 @@ class MyApp extends StatelessWidget {
     if (snapshot.hasError) {
       print('error occur while loading');
       return const Text('Error occur');
-    } else if (snapshot.hasData) {
+    } else if (snapshot.connectionState == ConnectionState.done) {
       return const TomatoApp();
     } else {
       return const SplashScreen();
@@ -61,13 +67,13 @@ class TomatoApp extends StatelessWidget {
       ],
       child: Builder(builder: (context) {
         final router = Provider.of<MyRouter>(context, listen: false).router;
-    return MaterialApp.router(
-      theme: ThemeData(
+        return MaterialApp.router(
+          theme: ThemeData(
             useMaterial3: false, // appbar 색상 변경하기 위해 추가
-        fontFamily: 'DoHyeon',
-        hintColor: Colors.grey[350],
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red),
+            fontFamily: 'DoHyeon',
+            hintColor: Colors.grey[350],
+            scaffoldBackgroundColor: Colors.white,
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red),
             textTheme: const TextTheme(
               labelLarge: TextStyle(color: Colors.white),
               bodyLarge: TextStyle(
@@ -79,24 +85,24 @@ class TomatoApp extends StatelessWidget {
                 fontSize: 13,
               ),
             ),
-        filledButtonTheme: FilledButtonThemeData(
-          style: FilledButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4),
-            ),
+            filledButtonTheme: FilledButtonThemeData(
+              style: FilledButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
                 minimumSize: const Size(48, 48),
-          ),
-        ),
-        appBarTheme: const AppBarTheme(
-          elevation: 2,
-          backgroundColor: Colors.white,
+              ),
+            ),
+            appBarTheme: const AppBarTheme(
+              elevation: 2,
+              backgroundColor: Colors.white,
               actionsIconTheme: IconThemeData(color: Colors.black87),
-        ),
+            ),
             bottomNavigationBarTheme: const BottomNavigationBarThemeData(
               selectedItemColor: Colors.black87,
               unselectedItemColor: Colors.black26,
             ),
-      ),
+          ),
           routerDelegate: router.routerDelegate,
           routeInformationProvider: router.routeInformationProvider,
           routeInformationParser: router.routeInformationParser,
